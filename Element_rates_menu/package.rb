11d57f97253ca67b419f53b8f60437cdc8b27091
FalconPilot <fpilot.github@gmail.com>
2015-12-27 11:33:32 +0100

    append package.rb

---
+++ Element_rates_menu/package.rb
@@ -0,0 +1,7 @@
+Package.new(
+    name: 'display-erates-menu',
+    version: vsn(1, 0, 0),
+    author: {'FalconPilot' => 'fpilot.github@gmail.com'},
+    components: ['display_erates.rb'],
+    description: 'Equip menu overhaul that displays element rates',
+)

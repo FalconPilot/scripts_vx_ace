d1bbd309321dddb760ddac16a07f81b79e3a847f
FalconPilot <fpilot.github@gmail.com>
2015-12-28 00:08:18 +0100

    removed placeholder script

--- Battle_menu_mugshots/package.rb
+++
@@ -1,7 +0,0 @@
-Package.new(
-    name: 'fpilot-combat-menu',
-    version: vsn(1, 0, 0),
-    authors: {'FalconPilot' => 'fpilot.github@gmail.com'},
-    components: [''],
-    description: 'Custom combat visual overhaul',
-)
